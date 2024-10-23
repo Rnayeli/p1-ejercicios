625fafc7e6030e8821a24a9dda8cd20dedd2d1f7
Nayeli <nayeli.rocha@davinci.edu.ar>
2024-10-23 17:03:53 -0300

    DEBES TERMINARLO

--- 2024-02-clase-02-ej-02/index.js
+++ 2024-02-clase-02-ej-02/index.js
@@ -3,67 +3,112 @@
 let ciudad, sexo, edad;
 let datoValido = true;
 
-//Validacion de ciudad
+//Variables para promedios
+let contadorMujeres = 0, contadorGeneral = 0, contadorHombres = 0, contadorNoBinario = 0;
+let acumuladorEdadMujeres = 0, acumuladorEdadGeneral = 0, acumuladorEdadHombres = 0, acumuladorEdadNoBinario = 0;
+
+//Variables para porcentajes
+let contadorMujeresMenores21 = 0, contadorHombresMayores21 = 0;
+
+//Variables para maximo y minimo de edad
+let edadHombreMax = 0, edadMujerMin = 110;
+
 do{
-    ciudad = prompt("Ingrese la ciudad");
-    if(!isNaN(ciudad)) {
-        alert("Ingrese texto, no numeros");
-        datoValido = false;
+    //Validacion de ciudad
+    do{
+        ciudad = prompt("Ingrese la ciudad");
+        if(!isNaN(ciudad)) {
+            alert("Ingrese texto, no numeros");
+            datoValido = false;
+        }
+        else if(ciudad === null) {
+            alert("Por favor, complete el campo");
+            datoValido = false;
+        }
+        else if(ciudad.trim() === "") {
+            alert("No dejar el prompt vacio");
+            datoValido = false;
+        }
+        else {
+            datoValido = true;
+        }
+    
+    } while(!datoValido);
+    
+    //validamos sexo
+
+    do{
+        sexo = prompt("Ingrese el sexo (H, M, X)");
+        if(!isNaN(sexo)) {
+            alert("Ingrese texto, no numeros");
+            datoValido = false;
+        }
+        else if(sexo === null) {
+            alert("Por favor, complete el campo");
+            datoValido = false;
+        }
+        else if(sexo.trim() === "") {
+            alert("No dejar el prompt vacio");
+            datoValido = false;
+        }
+        else if(sexo.toLowerCase() !== "h" && sexo.toLowerCase() !== "m" && sexo.toLowerCase() !== "x") {
+            alert("Complete con una opcion valida (H, M, X)");
+            datoValido = false;
+        }
+        else{
+            datoValido = true;
+        }
+    } while(!datoValido);
+    
+    //validamos edad
+
+    do{
+        edad = parseInt(prompt("Ingrese una edad entre 0 y 110"));
+    
+        if(isNaN(edad)) {
+            alert("Ingrese numeros");
+            datoValido = false;
+        }
+    
+        else if(edad < 0 || edad > 110) {
+            alert("Edad fuera de rango. Ingrese entre 0 y 110");
+            datoValido = false;
+        }
+        else{
+            datoValido = true;
+        }
+    } while(!datoValido);
+
+    //aca ya esta todo validado
+
+    //siempre incremento el contador general
+    contadorGeneral++;
+    acumuladorEdadGeneral += edad;
+
+    if(sexo.toLowerCase() === "h") {
+        //Es hombre
+        contadorHombres++;
+        acumuladorEdadHombres += edad;
+        //veo si cumple con el requisito de edad
+        if(edad > 21){
+            contadorHombresMayores21++;
+        }
     }
-    else if(ciudad === null) {
-        alert("Por favor, complete el campo");
-        datoValido = false;
-    }
-    else if(ciudad.trim() === "") {
-        alert("No dejar el prompt vacio");
-        datoValido = false;
+    else if (sexo.toLowerCase() === "m") {
+        //Es mujer
+        contadorMujeres++;
+        acumuladorEdadMujeres += edad;
+        //veo si cumple con el requisito de edad
+        if(edad < 21){
+            contadorMujeresMenores21++;
+        }
+
+        //Veo si es la mujer de mas edad
+        if(edad > edadMujerMin)
     }
     else {
-        datoValido = true;
+        //Es no binario
+        contadorNoBinario++;
+        acumuladorEdadNoBinario += edad;
     }
-
-} while(!datoValido);
-
-//validamos sexo
-
-do{
-    sexo = prompt("Ingrese el sexo (H, M, X)");
-    if(!isNaN(sexo)) {
-        alert("Ingrese texto, no numeros");
-        datoValido = false;
-    }
-    else if(sexo === null) {
-        alert("Por favor, complete el campo");
-        datoValido = false;
-    }
-    else if(sexo.trim() === "") {
-        alert("No dejar el prompt vacio");
-        datoValido = false;
-    }
-    else if(sexo.toLowerCase() !== "h" && sexo.toLowerCase() !== "m" && sexo.toLowerCase() !== "x") {
-        alert("Complete con una opcion valida (H, M, X)");
-        datoValido = false;
-    }
-    else{
-        datoValido = true;
-    }
-} while(!datoValido);
-
-//validamos edad
-
-do{
-    edad = parseInt(prompt("Ingrese una edad entre 0 y 110"));
-
-    if(isNaN(edad)) {
-        alert("Ingrese numeros");
-        datoValido = false;
-    }
-
-    else if(edad < 0 || edad > 110) {
-        alert("Edad fuera de rango. Ingrese entre 0 y 110");
-        datoValido = false;
-    }
-    else{
-        datoValido = true;
-    }
-} while(!datoValido);
+} while(confirm("Otro dato?"));
